36ad7f6c6ef20a666c1a94f59d2133d1814e42f2
EmirE <emirelci34@hotmail.com>
2022-07-02 22:21:48 +0300

    initial commit

---
+++ app/src/main/java/com/ee/einstagram/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ee.einstagram
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
